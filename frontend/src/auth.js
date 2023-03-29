2821d641c062e20c0728bb62e4f4f8c042e0f2b0
Elena Shcherbakova <25pochta25@mail.ru>
2023-03-29 13:20:37 +0300

    небольшие правки

--- frontend/src/auth.js
+++ frontend/src/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.REACT_APP_API_URL;
+export const BASE_URL = process.env.REACT_APP_API_URL ?? 'http://localhost:3000';
 
 
 export const register = ( email, password ) => {
